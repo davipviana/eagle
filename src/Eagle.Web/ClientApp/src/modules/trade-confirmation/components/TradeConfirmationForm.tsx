8d3d1abbeab9adc25d4cbc96a675c69e20e6c0e2
Davi Viana <davipviana@gmail.com>
2024-01-15 00:56:47 +0000

    wip tradeconfirmationform

---
+++ src/Eagle.Web/ClientApp/src/modules/trade-confirmation/components/TradeConfirmationForm.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '../../shared/components/Button/Button';
+
+export default function TradeConfirmationForm() {
+    return <div>
+        <Button>My Button</Button>
+    </div>;
+}
