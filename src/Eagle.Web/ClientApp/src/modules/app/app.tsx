ab96eac9df4f40c747fb0b236347575b1b9dd510
Davi Viana <davipviana@gmail.com>
2024-01-17 13:29:33 +0000

    wip - refactoring layout and add sidebar

--- src/Eagle.Web/ClientApp/src/modules/app/app.tsx
+++ src/Eagle.Web/ClientApp/src/modules/app/app.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import { AppLayout } from 'shared/components/layout';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import { AppLayout } from 'shared/layout';
 
 function App() {
   return (
