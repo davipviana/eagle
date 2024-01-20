1bf909e440daebc23bb0b5b179c39fc53e7e0692
Davi Viana <davipviana@gmail.com>
2024-01-20 17:31:17 -0300

    Application top bar and side bar with opening transition (#9)
    * wip appbar creation
    
    * reset layout and create app bar with menu toogle button
    
    * wip - refactoring layout and add sidebar

---
+++ src/Eagle.Web/ClientApp/src/shared/layout/hooks/useSidebarState.ts
@@ -0,0 +1,23 @@
+import { useStore } from 'shared/store';
+import { useMediaQuery, Theme } from '@mui/material';
+
+/**
+ * A hook that returns the sidebar open state and a function to toggle it.
+ *
+ * The sidebar is open by default on desktop, and closed by default on mobile.
+ *
+ * @example
+ * const ToggleSidebar = () => {
+ *     const [open, setOpen] = useSidebarState();
+ *     return (
+ *         <Button onClick={() => setOpen(!open)}>
+ *             {open ? 'Open' : 'Close'}
+ *         </Button>
+ *     );
+ * };
+ */
+export const useSidebarState = (): useSidebarStateResult => {
+    return useStore<boolean>('sidebar.open', false);
+};
+
+export type useSidebarStateResult = [boolean, (open: boolean) => void];
